33afde8737c162e479c6ac660fa6b31aac45f1b3
LeeJu-hyeong <010218jhlee@naver.com>
2021-09-23 00:38:27 +0900

    f

--- compile.py
+++ compile.py
@@ -2,21 +2,7 @@ import sys
 sys.stdin = open('in.txt', 'r')
 sys.stdout = open('out.txt','w')
 try:
-	n = int(sys.stdin.readline())
-
-	
-
-	ret = 0
-
-	
-
-	for i in range(n+1):
-
-		ret += i
-
-	
-
-	print("Sum from 1 to n : {}".format(ret))
+	adf
 
 except Exception as e:
 	print('Error Message: {}'.format(e))
